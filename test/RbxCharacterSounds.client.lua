0a66ea4a9d5f8dda5936c422e23d73e0ca8d9fe7
EgoMoose <EgoMoose@users.noreply.github.com>
2023-11-23 10:40:44 -0500

    New player listener

--- test/RbxCharacterSounds.client.lua
+++ test/RbxCharacterSounds.client.lua
@@ -4,26 +4,74 @@ local Players = game:GetService("Players")
 
 local CharacterSounds = require(game.ReplicatedStorage.Packages.CharacterSounds)
 
-local function doNothing() end
+local playerStates = {}
 
-local function onPlayerAdded(player: Player)
-	local cleanup = doNothing
-
-	local function onCharacterAdded(character: Model)
-		cleanup()
-		cleanup = CharacterSounds.listen(character)
+local function terminateSound(player: Player)
+	local state = playerStates[player] or {}
+	if state.terminateSound then
+		state.terminateSound()
+		state.terminateSound = nil
 	end
-
-	if player.Character then
-		onCharacterAdded(player.Character)
-	end
-
-	player.CharacterAdded:Connect(onCharacterAdded)
-	player.CharacterRemoving:Connect(function()
-		cleanup()
-		cleanup = doNothing
-	end)
 end
 
-onPlayerAdded(Players.LocalPlayer)
-Players.PlayerAdded:Connect(onPlayerAdded)
+local function characterRemoving(player: Player, _character: Model)
+	terminateSound(player)
+end
+
+local function characterAdded(player: Player, character: Model)
+	characterRemoving(player, character)
+
+	local state = playerStates[player]
+	if state then
+		state.terminateSound = CharacterSounds.listen(character)
+	end
+end
+
+local function playerRemoving(player: Player)
+	terminateSound(player)
+
+	local state = playerStates[player] or {}
+	for _, connection in state.connections or {} do
+		connection:Disconnect()
+	end
+
+	playerStates[player] = nil
+end
+
+local function playerAdded(player: Player)
+	playerRemoving(player)
+
+	local state = {
+		connections = {},
+		terminateSound = nil,
+	}
+
+	if player.Character then
+		characterAdded(player, player.Character)
+	end
+
+	table.insert(
+		state.connections,
+		player.CharacterAdded:Connect(function(character)
+			characterAdded(player, character)
+		end)
+	)
+
+	table.insert(
+		state.connections,
+		player.CharacterRemoving:Connect(function(character)
+			characterRemoving(player, character)
+		end)
+	)
+
+	playerStates[player] = state
+end
+
+----
+
+for _, player in Players:GetPlayers() do
+	task.spawn(playerAdded, player)
+end
+
+Players.PlayerAdded:Connect(playerAdded)
+Players.PlayerRemoving:Connect(playerRemoving)
